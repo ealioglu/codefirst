c33082b8656506d665d69ab1a6a3ee1a7eb340f8
403ENVER_SABAH <403ENVER_SABAH@DESKTOP-HNE43R2>
2022-05-25 13:48:39 +0300

    Add project files.

---
+++ codefirst/classlar/Urunler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codefirst.classlar
+{
+    public class Urunler
+    {
+        [Key]
+        public string _id { get; set; }
+        public string isim { get; set; }
+        public int adet { get; set; }
+        public string Marka { get; set; }
+    }
+}
